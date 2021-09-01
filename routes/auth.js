65e49e62b9392964135ce666c02fbfdccac25f0d
juliodevfullsatck <juliodevfullstack@gmail.com>
2021-09-01 17:07:29 -0300

    atualizando as validações e middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const {Router} = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 
-router.post('/new', crearUsuario );
+router.post(
+    '/new', 
+    [ // middlewares
+        check('name', 'O nome é ogrigatório').not().isEmpty(),
+        check('email', 'O email é obrigatório').isEmail(),
+        check('password', 'A senha deve ter 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    crearUsuario 
+);
 
-router.post('/', loginUsuario );
+router.post(
+    '/',
+    [
+        check('email', 'O email é obrigatório').isEmail(),
+        check('password', 'A senha deve ter 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+    ],
+    loginUsuario 
+);
 
 router.post('/renew', revalidarToken );
 
